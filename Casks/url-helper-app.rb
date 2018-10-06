b464f976c2e822d00c3a704b6b412c882c54862b
Grigory Entin <grigory.entin@gmail.com>
2018-10-06 20:14:48 +0200

    Added url-helper-app.

--- Casks/url-helper-app.rb
+++ Casks/url-helper-app.rb
@@ -1,8 +1,8 @@
 cask 'url-helper-app' do
   version '1'
-  sha256 '45cf203be009b1afb11be3c88feb1f95239c15e5be9649812b3255dee4548fc8'
+  sha256 '82dcfadb3e5a2aec201a30056a1b4a10e7f9a29a626aa73f576f906762721f73'
 
-  url 'https://gitlab.com/grigorye/URLHelperApp/-/jobs/105298479/artifacts/raw/build/URLHelperApp.dmg'
+  url 'https://gitlab.com/grigorye/URLHelperApp/-/jobs/105307646/artifacts/raw/build/URLHelperApp.dmg'
   name 'URLHelperApp'
   homepage 'https://gitlab.com/grigorye/URLHelperApp'
 
