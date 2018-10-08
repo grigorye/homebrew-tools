3a977afb4e92a1fae7848746fc89b8a26bb1a454
Grigory Entin <grigory.entin@gmail.com>
2018-10-08 09:47:11 +0200

    Updated casks.

--- Casks/simulator-recorder.rb
+++ Casks/simulator-recorder.rb
@@ -1,9 +1,9 @@
 cask 'simulator-recorder' do
-  version '285'
-  sha256 'ca0c81f2c70a199f843809205bd6840d259e353d823a33e677b6de60b245bcf4'
+  version '288'
+  sha256 'fec93408ab3c80fff283109073700abac94a15ad3cebf82bef2127ae375ba4d5'
 
-  url 'https://gitlab.com/grigorye/SimulatorRecorder/-/jobs/105421859/artifacts/raw/build/SimulatorRecorder.dmg'
-  name 'Simulator Recorder'
+  url 'https://gitlab.com/grigorye/SimulatorRecorder/-/jobs/105430669/artifacts/raw/build/SimulatorRecorder.dmg'
+  name 'SimulatorRecorder'
   homepage 'https://gitlab.com/grigorye/SimulatorRecorder'
 
   app 'SimulatorRecorder.app'
--- Casks/url-helper-app.rb
+++ Casks/url-helper-app.rb
@@ -1,8 +1,8 @@
 cask 'url-helper-app' do
-  version '26'
-  sha256 '6961a8c6655f4d29d8a93a5921c8ea55c229a4cf233d58cab373a4327607aaa1'
+  version '27'
+  sha256 '6769abe151d23879f3c77577af537d281d621440df3ad8124b1170dd51b012f4'
 
-  url 'https://gitlab.com/grigorye/URLHelperApp/-/jobs/105421061/artifacts/raw/build/URLHelperApp.dmg'
+  url 'https://gitlab.com/grigorye/URLHelperApp/-/jobs/105614263/artifacts/raw/build/URLHelperApp.dmg'
   name 'URLHelperApp'
   homepage 'https://gitlab.com/grigorye/URLHelperApp'
 
