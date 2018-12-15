3214cbbf31e37bbc067910daec7715a3caf64e74
Grigory Entin <grigory.entin@gmail.com>
2018-12-15 22:03:00 +0100

    Updated casks.

--- Casks/simulator-recorder.rb
+++ Casks/simulator-recorder.rb
@@ -1,8 +1,8 @@
 cask 'simulator-recorder' do
-  version '327'
-  sha256 '68f8d8c36c29967a31be47fe010d9a8adb0ff50c796f6f0e5bc19097337f2a6c'
+  version '328M'
+  sha256 '1c6c471c4f9dbf9cb864afd89807e7f47b26e293fbd36f161432b3df225b7bd5'
 
-  url 'https://gitlab.com/grigorye/SimulatorRecorder/-/jobs/134909443/artifacts/raw/build/SimulatorRecorder.dmg'
+  url 'https://gitlab.com/grigorye/SimulatorRecorder/-/jobs/135103336/artifacts/raw/build/SimulatorRecorder.dmg'
   name 'SimulatorRecorder'
   homepage 'https://gitlab.com/grigorye/SimulatorRecorder'
 
