4372b29a1c22fe38a2dc98b29c5bb36b83dfa49b
Grigory Entin <grigory.entin@gmail.com>
2022-01-14 02:02:50 +0100

    Added TMBuddy.

---
+++ time-machine-buddy.rb
@@ -0,0 +1,10 @@
+cask 'time-machine-buddy' do
+  version '76'
+  sha256 'f06fe2bc3c776c76e556d13915ca75772cdfed204205bce0f2205c2bd0a5a511'
+
+  url 'https://tmbuddy.s3.eu-west-2.amazonaws.com/TMBuddy-76.dmg'
+  name 'TMBuddy'
+  homepage 'https://github.com/grigorye/TMBuddy'
+
+  app 'TMBuddy.app'
+end
