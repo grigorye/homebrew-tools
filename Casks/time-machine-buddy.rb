d67c1ae9bd57e7157eb5880e3f1e162a68ab953d
Grigory Entin <grigory.entin@gmail.com>
2022-02-15 17:46:58 +0100

    Updated TMBuddy.

--- Casks/time-machine-buddy.rb
+++ Casks/time-machine-buddy.rb
@@ -1,8 +1,8 @@
 cask 'time-machine-buddy' do
-  version '409'
-  sha256 '18b7ec6285a5a033b98ce9d4561fd358118f16ee27b3234fc385f078bae60e01'
+  version '409.27.7'
+  sha256 'a2fee026a7a49d543ee242209880127ca5c28edd6c926a779c51ac9a045ca7e0'
 
-  url 'https://tmbuddy.s3.amazonaws.com/TMBuddy-409.dmg'
+  url 'https://tmbuddy.s3.amazonaws.com/TMBuddy-409.27.7.dmg'
   name 'TMBuddy'
   homepage 'https://github.com/grigorye/TMBuddy'
 
