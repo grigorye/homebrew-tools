2458b27c770e7bde086634e524686ca8b9ed6ede
Grigory Entin <grigory.entin@gmail.com>
2022-10-14 22:40:34 +0200

    Updated TMBuddy, with new bundle ID.

--- Casks/time-machine-buddy.rb
+++ Casks/time-machine-buddy.rb
@@ -1,8 +1,8 @@
 cask 'time-machine-buddy' do
-  version '627.64.18'
-  sha256 '4ce13a80371d380d3c115c8f1088be319265a84f54bd9639e67e5e616f61aeae'
+  version '1.0.82,505552.725.4'
+  sha256 'd8333035bdecdd56dca76565ba16655e0ca25ed06735632d42f801ddc55a1576'
 
-  url 'https://tmbuddy.s3.amazonaws.com/TMBuddy-627.64.18.dmg'
+  url 'https://tmbuddy.s3.amazonaws.com/TMBuddy-1.0.82-505552.725.4.dmg'
   name 'TMBuddy'
   homepage 'https://github.com/grigorye/TMBuddy'
 
