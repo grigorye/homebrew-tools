838cecbc501b1f04239effcbcfb7080509bebb92
Grigory Entin <grigory.entin@gmail.com>
2022-10-16 14:44:08 +0200

    Updated Timebox.

--- Casks/timebox.rb
+++ Casks/timebox.rb
@@ -1,6 +1,6 @@
 cask 'timebox' do
-  version 'macOS,app-0.1.9-655862.51.2'
-  sha256 'a29ac9c467e3d7eae03a1d77af81c36a846d3f1bf3c5a4f53b5604ab54c8d4a7'
+  version '0.1.9,655862.51.2'
+  sha256 'f7b430bef5278ada3b5eb3cb451e86a3250d626011817d639ab6a7453d1a1b01'
 
   url 'https://timeboxapp.s3.amazonaws.com/macOS-app-0.1.9-655862.51.2.dmg'
   name 'Timebox'
